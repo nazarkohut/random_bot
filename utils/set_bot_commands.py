ee1ef7e5a2df66fbdebd6b537b11d24de63e6e12
Nazar Kohut <nazarkohut0206@gmail.com>
2021-08-25 19:30:33 +0300

    Added random_emoji in utils/set_bot_commands, but this command won't work because the function for this command isn't created.

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,5 +7,7 @@ async def set_default_commands(dp):
             types.BotCommand("start", "Start Bot"),
             types.BotCommand("help", "shows commands list"),
             types.BotCommand("random_integer", "Generate random integer"),
+            types.BotCommand("random_answer", 'Will random Yes or No'),
+            types.BotCommand("random_emoji", 'will random emojis')
         ]
     )
