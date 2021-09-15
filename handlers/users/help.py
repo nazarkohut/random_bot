767576650b6ee12b95efd644bca9a0c6075ee746
Nazar Kohut <nazarkohut0206@gmail.com>
2021-09-15 05:41:05 +0300

    Refactored text for commands.

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -5,7 +5,17 @@ from loader import dp
 @dp.message_handler(commands='help')
 async def bot_help(message: types.Message):
     text = ("Commands: ",
-            "/start - Start Bot",
-            "/help - Get commands list",
+            "/start - start bot",
+            "/help - get commands list",
+            "/random_integer - generate random integer",
+            "/random_answer - random Yes or No",
+            "/random_emoji - sends random emoji",
+            "/random_country - random country by the national flag",
+            "/random_image - random beautiful image",
+            "/random_gif - sends random gif",
+            "/random_mem - will random mem",
+            "/random_color - random color and its hex code",
+            "/random_my_choice - random one object from the sequence you've typed",
+            "/random_admin - get random group administrator",
             )
     await message.answer("\n".join(text))
